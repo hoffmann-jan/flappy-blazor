4a9cc15a0feddbd3e97e33f3bb0cb7747c5e821b
hoffmann-jan <34597495+hoffmann-jan@users.noreply.github.com>
2022-03-29 12:05:14 +0200

    add spinner and button

---
+++ src/FlappyBlazorGame/BlazorGames.Wasm/Components/DoubleBorderButton.razor.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace BlazorGames.Wasm.Components;
+public partial class DoubleBorderButton
+{
+    [Parameter] public string Caption { get; set; } = string.Empty;
+    [Parameter] public string Href { get; set; } = "#";
+}
