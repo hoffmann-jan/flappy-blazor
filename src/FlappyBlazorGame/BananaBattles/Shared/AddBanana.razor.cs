73e99960df41574da61ca75827e7197a7786aa4d
hoffmann-jan <34597495+hoffmann-jan@users.noreply.github.com>
2022-05-01 16:28:47 +0200

    added add banana function

---
+++ src/FlappyBlazorGame/BananaBattles/Shared/AddBanana.razor.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace BananaBattles.Shared;
+
+public partial class AddBanana
+{
+    [Parameter]
+    public EventCallback<int> BananasAdded { get; set; }
+
+    public async Task IncreaseBananaCount()
+    {
+        await BananasAdded.InvokeAsync(10);
+    }
+}
--- src/FlappyBlazorGame/BananaBattles/Shared/TopMenu.razor.cs
+++ src/FlappyBlazorGame/BananaBattles/Shared/TopMenu.razor.cs
@@ -4,5 +4,10 @@ namespace BananaBattles.Shared;
 public partial class TopMenu
 {
     [Parameter]
-    public uint Bananas { get; set; } = 100;
+    public int Bananas { get; set; } = 100;
+
+    public void AddBananas(int value)
+    {
+        Bananas += value;
+    }
 }
