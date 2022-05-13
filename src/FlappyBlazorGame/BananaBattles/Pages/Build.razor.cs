fda0283e92152d8c1dcf37dd0e49f30e7bca04ab
hoffmann-jan <34597495+hoffmann-jan@users.noreply.github.com>
2022-05-13 21:13:44 +0200

    fix flexbox

---
+++ src/FlappyBlazorGame/BananaBattles/Pages/Build.razor.cs
@@ -0,0 +1,11 @@
+﻿namespace BananaBattles.Pages;
+
+public partial class Build
+{
+    private void ConsumeBananas(int amount)
+    {
+        Console.WriteLine($"{amount} bananas eaten.");
+    }
+
+
+}
